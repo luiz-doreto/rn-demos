a0dc72892f4dc27d658894080a52cbf5d2689de7
Luiz Doreto <luiz.doreto14@gmail.com>
2025-10-28 10:53:54 -0300

    use reusables cli to create a new project

--- rn-reusables/app/index.tsx
+++ rn-reusables/app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { SignInForm } from '@/components/sign-in-form';
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+import { Stack } from 'expo-router';
+import { MoonStarIcon, SunIcon } from 'lucide-react-native';
+import { useColorScheme } from 'nativewind';
+import * as React from 'react';
+import { ScrollView, View } from 'react-native';
 
-export default function Index() {
+const SCREEN_OPTIONS = {
+  title: '',
+  headerTransparent: true,
+  headerRight: () => <ThemeToggle />,
+};
+
+export default function Screen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <Stack.Screen options={SCREEN_OPTIONS} />
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe"
+        keyboardDismissMode="interactive">
+        <View className="w-full max-w-sm">
+          <SignInForm />
+        </View>
+      </ScrollView>
+    </>
+  );
+}
+
+const THEME_ICONS = {
+  light: SunIcon,
+  dark: MoonStarIcon,
+};
+
+function ThemeToggle() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <Button
+      onPressIn={toggleColorScheme}
+      size="icon"
+      variant="ghost"
+      className="ios:size-9 rounded-full web:mx-4">
+      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
+    </Button>
   );
 }
